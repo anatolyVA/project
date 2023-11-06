6d08d218d721879cb899e0647d5e10c47e6369d9
anatolyVA <nsukhankov@inbox.ru>
2023-11-07 01:16:42 +0300

    Обновляет zoom в Map.jsx

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -9,7 +9,7 @@ export default function Map() {
     load().then((mapglAPI) => {
       map = new mapglAPI.Map("map-container", {
         center: [54.78782130341394, 32.05245062677451],
-        zoom: 13,
+        zoom: 9,
         key: "5aefc340-8c74-4047-ab92-dbb80efe4d8c",
       });
     });
